346558bc2f1b00b4d8c3dd03bb5f35108c72c2b6
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-02 00:24:30 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,7 +1,7 @@
 export default function manageBand(state = {bands: []}, action) {
   switch (action.type){
     case 'ADD_BAND':
-      return {...state, bands: [...state.bands, action.band]}
+      return state.bands.concat(action.band)
     default:
       return state;
   }
